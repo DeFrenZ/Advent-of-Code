751d6b27fe1cc4086005beafe17f3472a7a19dfc
Davide De Franceschi <defrenz@me.com>
2020-12-12 19:32:25 +0000

    Add proper formatting to the days descriptions

--- Sources/AdventOfCode/2020/Day4Year2020.swift
+++ Sources/AdventOfCode/2020/Day4Year2020.swift
@@ -11,8 +11,8 @@ public final class Day4Year2020: DaySolverWithInputs {
         self.inputElements = inputElements
     }
 
-    /*
-     --- Day 4: Passport Processing ---
+    /**
+     # --- Day 4: Passport Processing ---
 
      You arrive at the airport only to realize that you grabbed your North Pole Credentials instead of your passport. While these documents are extremely similar, North Pole Credentials aren't issued by a country and therefore aren't actually valid documentation for travel in most of the world.
 
@@ -20,21 +20,22 @@ public final class Day4Year2020: DaySolverWithInputs {
 
      Due to some questionable network security, you realize you might be able to solve both of these problems at the same time.
 
-     The automatic passport scanners are slow because they're having trouble detecting which passports have all required fields. The expected fields are as follows:
+     The automatic passport scanners are slow because they're having trouble **detecting which passports have all required fields**. The expected fields are as follows:
 
-     - byr (Birth Year)
-     - iyr (Issue Year)
-     - eyr (Expiration Year)
-     - hgt (Height)
-     - hcl (Hair Color)
-     - ecl (Eye Color)
-     - pid (Passport ID)
-     - cid (Country ID)
+     - `byr` (Birth Year)
+     - `iyr` (Issue Year)
+     - `eyr` (Expiration Year)
+     - `hgt` (Height)
+     - `hcl` (Hair Color)
+     - `ecl` (Eye Color)
+     - `pid` (Passport ID)
+     - `cid` (Country ID)
 
-     Passport data is validated in batch files (your puzzle input). Each passport is represented as a sequence of key:value pairs separated by spaces or newlines. Passports are separated by blank lines.
+     Passport data is validated in batch files (your puzzle input). Each passport is represented as a sequence of `key:value` pairs separated by spaces or newlines. Passports are separated by blank lines.
 
      Here is an example batch file containing four passports:
 
+     ```
      ecl:gry pid:860033327 eyr:2020 hcl:#fffffd
      byr:1937 iyr:2017 cid:147 hgt:183cm
 
@@ -48,16 +49,17 @@ public final class Day4Year2020: DaySolverWithInputs {
 
      hcl:#cfa07d eyr:2025 pid:166559648
      iyr:2011 ecl:brn hgt:59in
+     ```
 
-     The first passport is valid - all eight fields are present. The second passport is invalid - it is missing hgt (the Height field).
+     The first passport is **valid** - all eight fields are present. The second passport is **invalid** - it is missing hgt (the Height field).
 
-     The third passport is interesting; the only missing field is cid, so it looks like data from North Pole Credentials, not a passport at all! Surely, nobody would mind if you made the system temporarily ignore missing cid fields. Treat this "passport" as valid.
+     The third passport is interesting; the **only missing field** is `cid`, so it looks like data from North Pole Credentials, not a passport at all! Surely, nobody would mind if you made the system temporarily ignore missing `cid` fields. Treat this "passport" as **valid**.
 
-     The fourth passport is missing two fields, cid and byr. Missing cid is fine, but missing any other field is not, so this passport is invalid.
+     The fourth passport is missing two fields, `cid` and `byr`. Missing `cid` is fine, but missing any other field is not, so this passport is **invalid**.
 
-     According to the above rules, your improved system would report 2 valid passports.
+     According to the above rules, your improved system would report **`2`** valid passports.
 
-     Count the number of valid passports - those that have all required fields. Treat cid as optional. In your batch file, how many passports are valid?
+     Count the number of **valid** passports - those that have all required fields. Treat `cid` as optional. **In your batch file, how many passports are valid?**
      */
     public func solvePart1() -> String {
         inputElements
@@ -65,25 +67,27 @@ public final class Day4Year2020: DaySolverWithInputs {
             .description
     }
 
-    /*
-     --- Part Two ---
+    /**
+     # --- Part Two ---
 
      The line is moving more quickly now, but you overhear airport security talking about how passports with invalid data are getting through. Better add some data validation, quick!
 
      You can continue to ignore the cid field, but each other field has strict rules about what values are valid for automatic validation:
 
-     byr (Birth Year) - four digits; at least 1920 and at most 2002.
-     iyr (Issue Year) - four digits; at least 2010 and at most 2020.
-     eyr (Expiration Year) - four digits; at least 2020 and at most 2030.
-     hgt (Height) - a number followed by either cm or in:
-     If cm, the number must be at least 150 and at most 193.
-     If in, the number must be at least 59 and at most 76.
-     hcl (Hair Color) - a # followed by exactly six characters 0-9 or a-f.
-     ecl (Eye Color) - exactly one of: amb blu brn gry grn hzl oth.
-     pid (Passport ID) - a nine-digit number, including leading zeroes.
-     cid (Country ID) - ignored, missing or not.
-     Your job is to count the passports where all required fields are both present and valid according to the above rules. Here are some example values:
+     - `byr` (Birth Year) - four digits; at least `1920` and at most `2002`.
+     - `iyr` (Issue Year) - four digits; at least `2010` and at most `2020`.
+     - `eyr` (Expiration Year) - four digits; at least `2020` and at most `2030`.
+     - `hgt` (Height) - a number followed by either `cm` or `in`:
+        - If `cm`, the number must be at least `150` and at most `193`.
+        - If `in`, the number must be at least `59` and at most `76`.
+     - `hcl` (Hair Color) - a `#` followed by exactly six characters `0`-`9` or `a`-`f`.
+     - `ecl` (Eye Color) - exactly one of: `amb` `blu` `brn` `gry` `grn` `hzl` `oth`.
+     - `pid` (Passport ID) - a nine-digit number, including leading zeroes.
+     - `cid` (Country ID) - ignored, missing or not.
 
+     Your job is to count the passports where all required fields are both **present** and **valid** according to the above rules. Here are some example values:
+
+     ```
      byr valid:   2002
      byr invalid: 2003
 
@@ -101,9 +105,11 @@ public final class Day4Year2020: DaySolverWithInputs {
 
      pid valid:   000000001
      pid invalid: 0123456789
+     ```
 
      Here are some invalid passports:
 
+     ```
      eyr:1972 cid:100
      hcl:#18171d ecl:amb hgt:170 pid:186cm iyr:2018 byr:1926
 
@@ -117,8 +123,11 @@ public final class Day4Year2020: DaySolverWithInputs {
      hgt:59cm ecl:zzz
      eyr:2038 hcl:74454a iyr:2023
      pid:3556412378 byr:2007
+     ```
+
      Here are some valid passports:
 
+     ```
      pid:087499704 hgt:74in ecl:grn iyr:2012 eyr:2030 byr:1980
      hcl:#623a2f
 
@@ -131,8 +140,9 @@ public final class Day4Year2020: DaySolverWithInputs {
      eyr:2022
 
      iyr:2010 hgt:158cm hcl:#b6652a ecl:blu byr:1944 eyr:2021 pid:093154719
+     ```
 
-     Count the number of valid passports - those that have all required fields and valid values. Continue to treat cid as optional. In your batch file, how many passports are valid?
+     Count the number of **valid** passports - those that have all required fields **and valid values**. Continue to treat `cid` as optional. **In your batch file, how many passports are valid?**
      */
     public func solvePart2() -> String {
         inputElements
--- Sources/AdventOfCode/2020/Day6Year2020.swift
+++ Sources/AdventOfCode/2020/Day6Year2020.swift
@@ -1,10 +1,3 @@
-//
-//  File.swift
-//  
-//
-//  Created by Davide De Franceschi on 06/12/2020.
-//
-
 import Foundation
 
 public final class Day6Year2020: DaySolverWithInputs {
@@ -18,23 +11,26 @@ public final class Day6Year2020: DaySolverWithInputs {
         self.inputElements = inputElements
     }
 
-    /*
-     --- Day 6: Custom Customs ---
+    /**
+     # --- Day 6: Custom Customs ---
 
-     As your flight approaches the regional airport where you'll switch to a much larger plane, customs declaration forms are distributed to the passengers.
+     As your flight approaches the regional airport where you'll switch to a much larger plane, [https://en.wikipedia.org/wiki/Customs_declaration](customs declaration forms) are distributed to the passengers.
 
-     The form asks a series of 26 yes-or-no questions marked a through z. All you need to do is identify the questions for which anyone in your group answers "yes". Since your group is just you, this doesn't take very long.
+     The form asks a series of 26 yes-or-no questions marked `a` through `z`. All you need to do is identify the questions for which **anyone in your group** answers "yes". Since your group is just you, this doesn't take very long.
 
      However, the person sitting next to you seems to be experiencing a language barrier and asks if you can help. For each of the people in their group, you write down the questions for which they answer "yes", one per line. For example:
 
+     ```
      abcx
      abcy
      abcz
+     ```
 
-     In this group, there are 6 questions to which anyone answered "yes": a, b, c, x, y, and z. (Duplicate answers to the same question don't count extra; each question counts at most once.)
+     In this group, there are **`6`** questions to which anyone answered "yes": `a`, `b`, `c`, `x`, `y`, and `z`. (Duplicate answers to the same question don't count extra; each question counts at most once.)
 
      Another group asks for your help, then another, and eventually you've collected answers from every group on the plane (your puzzle input). Each group's answers are separated by a blank line, and within each group, each person's answers are on a single line. For example:
 
+     ```
      abc
 
      a
@@ -50,18 +46,19 @@ public final class Day6Year2020: DaySolverWithInputs {
      a
 
      b
+     ```
 
      This list represents answers from five groups:
 
-     - The first group contains one person who answered "yes" to 3 questions: a, b, and c.
-     - The second group contains three people; combined, they answered "yes" to 3 questions: a, b, and c.
-     - The third group contains two people; combined, they answered "yes" to 3 questions: a, b, and c.
-     - The fourth group contains four people; combined, they answered "yes" to only 1 question, a.
-     - The last group contains one person who answered "yes" to only 1 question, b.
+     - The first group contains one person who answered "yes" to **`3`** questions: `a`, `b`, and `c`.
+     - The second group contains three people; combined, they answered "yes" to **`3`** questions: `a`, `b`, and `c`.
+     - The third group contains two people; combined, they answered "yes" to **`3`** questions: `a`, `b`, and `c`.
+     - The fourth group contains four people; combined, they answered "yes" to only **`1`** question, `a`.
+     - The last group contains one person who answered "yes" to only **`1`** question, `b`.
 
-     In this example, the sum of these counts is 3 + 3 + 3 + 1 + 1 = 11.
+     In this example, the sum of these counts is `3 + 3 + 3 + 1 + 1` = **`11`**.
 
-     For each group, count the number of questions to which anyone answered "yes". What is the sum of those counts?
+     For each group, count the number of questions to which anyone answered "yes". **What is the sum of those counts?**
      */
     public func solvePart1() -> String {
         inputElements
@@ -70,15 +67,16 @@ public final class Day6Year2020: DaySolverWithInputs {
             .description
     }
 
-    /*
-     --- Part Two ---
+    /**
+     # --- Part Two ---
 
      As you finish the last group's customs declaration, you notice that you misread one word in the instructions:
 
-     You don't need to identify the questions to which anyone answered "yes"; you need to identify the questions to which everyone answered "yes"!
+     You don't need to identify the questions to which **anyone** answered "yes"; you need to identify the questions to which **everyone** answered "yes"!
 
      Using the same example as above:
 
+     ```
      abc
 
      a
@@ -94,18 +92,19 @@ public final class Day6Year2020: DaySolverWithInputs {
      a
 
      b
+     ```
 
      This list represents answers from five groups:
 
-     - In the first group, everyone (all 1 person) answered "yes" to 3 questions: a, b, and c.
-     - In the second group, there is no question to which everyone answered "yes".
-     - In the third group, everyone answered yes to only 1 question, a. Since some people did not answer "yes" to b or c, they don't count.
-     - In the fourth group, everyone answered yes to only 1 question, a.
-     - In the fifth group, everyone (all 1 person) answered "yes" to 1 question, b.
+     - In the first group, everyone (all 1 person) answered "yes" to **`3`** questions: `a`, `b`, and `c`.
+     - In the second group, there is **no** question to which everyone answered "yes".
+     - In the third group, everyone answered yes to only **`1`** question, `a`. Since some people did not answer "yes" to `b` or `c`, they don't count.
+     - In the fourth group, everyone answered yes to only **`1`** question, `a`.
+     - In the fifth group, everyone (all 1 person) answered "yes" to **`1`** question, `b`.
 
-     In this example, the sum of these counts is 3 + 0 + 1 + 1 + 1 = 6.
+     In this example, the sum of these counts is `3 + 0 + 1 + 1 + 1` = **`6`**.
 
-     For each group, count the number of questions to which everyone answered "yes". What is the sum of those counts?
+     For each group, count the number of questions to which **everyone** answered "yes". **What is the sum of those counts?**
      */
     public func solvePart2() -> String {
         inputElements
